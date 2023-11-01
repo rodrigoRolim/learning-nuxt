568e66476294dbaa4a5410971c202fa41c403826
rodrigoRolim <rodrigorolimveras92@gmail.com>
2023-11-01 14:00:07 -0300

    criar meta tag e página de erro

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <Head>
+      <Title>Nuxt Dojo | {{ product.title }}</Title>
+      <Meta name="description" :content="product.description" />
+    </Head>
+
     <ProductDetails :product="product"/>
   </div>
 </template>
@@ -9,6 +14,10 @@ const { id } = useRoute().params
 const uri = 'https://fakestoreapi.com/products/' + id
 
 const { data: product } = await useFetch(uri, { key: id })
+
+if (!product.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+}
 
 definePageMeta({
   layout: 'products'
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -15,6 +15,12 @@ definePageMeta({
 
 const { data: products } = await useFetch('https://fakestoreapi.com/products');
 
+useHead({
+  title: 'Nuxt Dojo | Merch',
+  meta: [
+    { name: 'description', content: 'Nuxt 3 Merch' }
+  ]
+})
 </script>
 
 <style scoped>
